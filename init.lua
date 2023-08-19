75f29f5c255b66a469dcf61b438dd44bc9b4b607
Aronax79 <102997588+Aronax79@users.noreply.github.com>
2023-08-19 18:30:43 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ if oh then
 end
 
 local web = true
-local user = "Upbolt" -- change if you're using a fork
+local user = "Aronax79" -- change if you're using a fork
 local branch = "revision"
 local importCache = {}
 
