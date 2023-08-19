fadd5e84489722ea12c4591929255168691ed8e6
Aronax79 <102997588+Aronax79@users.noreply.github.com>
2023-08-19 18:35:41 +0200

    Update ScriptScanner.lua

--- ui/modules/ScriptScanner.lua
+++ ui/modules/ScriptScanner.lua
@@ -137,7 +137,7 @@ function Log.new(localScript)
     button.Name = scriptName
     button:FindFirstChild("Name").Text = scriptName
     button.Protos.Text = #localScript.Protos
-    if #localScript.Constants ~= nil then
+    if localScript.Constants ~= nil then
         button.Constants.Text = #localScript.Constants
     end
     listButton:SetCallback(function()
