37bc5cae87dd3b7319d546028366eafb2583903b
Aronax79 <102997588+Aronax79@users.noreply.github.com>
2023-08-20 22:05:03 +0200

    Update ModuleScript.lua

--- objects/ModuleScript.lua
+++ objects/ModuleScript.lua
@@ -4,10 +4,12 @@ function ModuleScript.new(instance)
     local moduleScript = {}
     local closure = getScriptClosure(instance)
 
-    moduleScript.Instance = instance
-    moduleScript.Constants = getConstants(closure)
-    moduleScript.Protos = getProtos(closure)
-    --moduleScript.ReturnValue = require(instance) // causes detection
+    if closure ~= nil then
+        moduleScript.Instance = instance
+        moduleScript.Constants = getConstants(closure)
+        moduleScript.Protos = getProtos(closure)
+        --moduleScript.ReturnValue = require(instance) // causes detection
+    end
 
     return moduleScript
 end
